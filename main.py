2a85647e8cc4ee146b1492e05e5806d974b9a396
rnjjkk <jamesnaritnk@gmail.com>
2026-01-15 14:22:56 +0700

    add git commands

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 print("main")
 print("itkuyyai007")
 print("kenny")
+
+print("" \
+"git init\n" \
+"git remote add origin <url>\n" \
+"git remote origin -v\n" \
+"git status\n" \
+"git add <file.name> / add . (all files)\n" \
+"git commit -m \"<message>\"\n" \
+"git log\n" \
+"git reset HEAD~1\n" \
+"git branch\n" \
+"git branch <name>\n" \
+"git checkout <name>/<hash>\n" \
+"git push origin <name> / git push origin --all (all branches)\n" \
+"git merge <name> (merge <name> to HEAD\n" \
+"git pull origin <name> / git pull origin --all (all branches))")
